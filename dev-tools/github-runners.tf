d8007f1839eacae9cdf42b0ae5d0ec754078a3ab
James Pruitt <jamescpruitt@gmail.com>
2026-04-27 22:01:59 -0600

    add prometheus to gh runners

--- dev-tools/github-runners.tf
+++ dev-tools/github-runners.tf
@@ -8,6 +8,8 @@ resource "helm_release" "arc" {
   chart            = "oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller"
   namespace        = "arc-systems"
   create_namespace = true
+
+  values = [ file("${path.module}/github-controller.yml") ]
 }
 
 resource "helm_release" "homelab" {
@@ -16,23 +18,9 @@ resource "helm_release" "homelab" {
   namespace        = "arc-systems"
   create_namespace = true
 
-  values = [
-    yamlencode({
-      githubConfigUrl = "https://github.com/Rapdorian/homelab"
-      githubConfigSecret = {
-        github_token = var.github_token
-      }
-      metrics = {
-        enabled = true
-        proxy = {
-          enabled = false
-        }
-        serviceMonitor = {
-          enabled = true
-        }
-      }
-    })
-  ]
+  values = [ templatefile("${path.module}/github-runners.yml", {
+    token = var.github_token
+  }) ]
 
   depends_on = [helm_release.arc]
 }
